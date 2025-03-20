f238a72435e0902f1a85dfa8d02b67bfa22deb8a
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2025-03-20 12:16:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mis_widgets_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mis_widgets_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mis_widgets_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
