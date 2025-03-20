6ab0296677328377e037cad9230be588be478c9f
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2025-03-20 12:55:25 -0500

    Home Screen creado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mis_widgets_app/config/theme/app_theme.dart';
+import 'package:mis_widgets_app/presentatio/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 0).getTheme(),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: HomeScreen(),
       ),
     );
   }
