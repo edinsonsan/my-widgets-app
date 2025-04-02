29cd2165d270622042c63eecfb9a6c72d7b96e71
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2025-04-02 07:52:48 -0500

    Cambiar valores y modificadores en pantalla contador usando riverpod

--- lib/presentatio/screens/counter/counterScreen.dart
+++ lib/presentatio/screens/counter/counterScreen.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mis_widgets_app/presentatio/providers/counter_provider.dart';
+import 'package:mis_widgets_app/presentatio/providers/theme_provider.dart';
 
-class CounterScreeen extends StatelessWidget {
+class CounterScreeen extends ConsumerWidget {
   static const String name = 'counter_screen';
   const CounterScreeen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkmode = ref.watch(isDarkmodeProvider);
     return Scaffold(
-      appBar: AppBar(title: const Text('Pantalla Contador')),
-      body: Center(child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge,)),
+      appBar: AppBar(
+        title: const Text('Pantalla Contador'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              isDarkmode ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
+            ),
+            // icon: const Icon(Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Valor: $clickCounter',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+          ref.read(counterProvider.notifier).state++;
+        },
         child: const Icon(Icons.add),
       ),
     );
