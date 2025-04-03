190c2b244db57807804f0a1353c8fb9e90d2a080
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2025-04-03 11:16:34 -0500

    terminando la pantalla del contador usando riverpod y stateNotifier para cambiar el tema de la aplicación

--- lib/presentatio/screens/counter/counter_screen.dart
+++ lib/presentatio/screens/counter/counter_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mis_widgets_app/presentatio/providers/counter_provider.dart';
 import 'package:mis_widgets_app/presentatio/providers/theme_provider.dart';
 
@@ -10,7 +11,8 @@ class CounterScreeen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
-    final bool isDarkmode = ref.watch(isDarkmodeProvider);
+    // final bool isDarkmode = ref.watch(isDarkmodeProvider);
+    final bool isDarkmode = ref.watch(themeNotifierProvider).isDarkmode;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pantalla Contador'),
@@ -21,23 +23,72 @@ class CounterScreeen extends ConsumerWidget {
             ),
             // icon: const Icon(Icons.light_mode_outlined),
             onPressed: () {
-              ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+              // ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+              ref.read(themeNotifierProvider.notifier).toggleDarkMode();
             },
           ),
         ],
       ),
       body: Center(
-        child: Text(
-          'Valor: $clickCounter',
-          style: Theme.of(context).textTheme.titleLarge,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+            Text(
+              'Click${clickCounter == 1 ? '' : 's'}',
+              style: const TextStyle(fontSize: 25),
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // ref.read(counterProvider.notifier).update((state) => state + 1);
-          ref.read(counterProvider.notifier).state++;
-        },
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconButton.filledTonal(
+            onPressed: () {
+              // ref.read(counterProvider.notifier).update((state) => state + 1);
+              ref.read(counterProvider.notifier).state++;
+            },
+            icon: const Icon(Icons.add),
+          ),
+
+          const SizedBox(height: 10),
+
+          IconButton.filledTonal(
+            onPressed: () {
+              // ref.read(counterProvider.notifier).update((state) => state + 1);
+
+              if (clickCounter <= 0) return;
+              ref.read(counterProvider.notifier).state--;
+            },
+            icon: const Icon(Icons.exposure_minus_1_outlined),
+          ),
+
+          const SizedBox(height: 10),
+
+          IconButton.filledTonal(
+            onPressed: () {
+              // ref.read(counterProvider.notifier).update((state) => state + 1);
+              ref.read(counterProvider.notifier).state = 0;
+            },
+            icon: const Icon(Icons.refresh_outlined),
+          ),
+
+          const SizedBox(height: 10),
+
+          FloatingActionButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: const Icon(Icons.arrow_back_ios_new_outlined),
+          ),
+        ],
       ),
     );
   }
