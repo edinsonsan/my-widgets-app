76dd1c8f01fe7f61a564fc6d1241b518f532425f
Edinson Sandoval <sandovalescobaredinson151@gmail.com>
2025-04-03 11:19:19 -0500

    todo con 0 advertencias y errores

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mis_widgets_app/config/router/app_router.dart';
-import 'package:mis_widgets_app/config/theme/app_theme.dart';
 import 'package:mis_widgets_app/presentatio/providers/theme_provider.dart';
 
 void main() {
